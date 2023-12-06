1af45a1ae4cb3f946831ca9d8a4a0c3933dc9914
annamani <annamanik.vit@gmail.com>
2023-12-06 21:47:32 +0100

    ex2

--- Day -02/StringOps.py
+++ Day -02/StringOps.py
@@ -11,3 +11,8 @@ print(len(first_name))
 print(Country.lower())
 # String Upper Case
 print(first_name.upper())
+
+# String Replace
+text = "Denmark is a big country"
+new_text = text.replace("big", "small")
+print("Modified text:", new_text)
