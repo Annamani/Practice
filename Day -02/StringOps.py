7f1733803fb84f17129dd9398a1043f5236e825d
annamani <annamanik.vit@gmail.com>
2023-12-06 21:48:22 +0100

    ex2

--- Day -02/StringOps.py
+++ Day -02/StringOps.py
@@ -16,3 +16,8 @@ print(first_name.upper())
 text = "Denmark is a big country"
 new_text = text.replace("big", "small")
 print("Modified text:", new_text)
+
+# String Strips
+sample = "   Some spaces around   "
+stripped_text = sample.strip()
+print("Stripped text:", stripped_text)
