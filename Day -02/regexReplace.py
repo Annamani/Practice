b008211b3d49b6215fe859e764ee92d57bda7c3b
annamani <annamanik.vit@gmail.com>
2023-12-07 20:09:07 +0100

    ex5

--- Day -02/regexReplace.py
+++ Day -02/regexReplace.py
@@ -4,6 +4,19 @@ text = "The quick brown fox jumps over the lazy brown dog"
 pattern = r"brown"
 
 replacement = "red"
-
 new_text = re.sub(pattern, replacement, text)
 print("Modified text:", new_text)
+
+# Search for a key
+search = re.search(pattern, text)
+if search:
+    print("Pattern found:", search.group())
+else:
+    print("Pattern not found")
+
+# Split Text
+textex = "apple,banana,orange,grape"
+patternex = r","
+
+split_result = re.split(patternex, textex)
+print("Split result:", split_result)
