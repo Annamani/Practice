35bc57b6e5d703286d6bb8b1c3e811d25c264312
annamani <annamanik.vit@gmail.com>
2023-12-13 21:35:48 +0100

    variables

--- Day -02/multipleVariables.py
+++ Day -02/multipleVariables.py
@@ -1,4 +1,10 @@
+# Assign multiple variables
 name,age,address="Annamani",26," '''copenhagen,Denmark''' "
 print(name)
 print(age)
 print(address)
+
+# assign the same value to multiple variables 
+location=capital="Denmark"
+print(location)
+print(capital)
