4915fb834a7441322b514127b93e6d03bb56ee97
annamani <annamanik.vit@gmail.com>
2023-12-15 16:25:03 +0100

    examples

--- Day -02/examples.py
+++ Day -02/examples.py
@@ -2,3 +2,10 @@ number1=9
 number2=5
 total=number1+number2
 print(total)
+
+# Take inputs
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+sum = float(num1) + float(num2)
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
