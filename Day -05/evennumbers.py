3b7674ddc2acb88c38f848c8920c84f7ad90f4a9
annamani <annamanik.vit@gmail.com>
2023-12-28 19:40:14 +0100

    even

--- Day -05/evennumbers.py
+++ Day -05/evennumbers.py
@@ -1,6 +1,7 @@
 number=input("enter the number: ")
+result=False
 for i in range(1,21):
     if i % 2==0:
-        print(i)
+        result=True
     else:
-        print("It's not an even number")
+         result=False
