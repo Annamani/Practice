1f72494395421a53208c224e9468222c53d5abea
annamani <annamanik.vit@gmail.com>
2024-01-02 16:12:30 +0100

    assignment

--- Day -05/exercise.py
+++ Day -05/exercise.py
@@ -5,6 +5,16 @@
 #     number+=1
 
 # For Loop
-for number in range(1,21):
-    if number%2==0:
-        print(number)
+# for number in range(1,21):
+#     if number%2==0:
+#         print(number)
+
+num= int(input("Enter number: "))
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
