ea4822363585f34b234660a285c264dee6752f1f
annamani <annamanik.vit@gmail.com>
2024-01-02 16:22:09 +0100

    assignment

--- Day -05/funcassign.py
+++ Day -05/funcassign.py
@@ -1,8 +1,15 @@
-def calculation(a,b,sign):
-    if sign=="+":
-        return a+b
-    elif sign=="-":
-        return a-b
+# def calculation(a,b,sign):
+#     if sign=="+":
+#         return a+b
+#     elif sign=="-":
+#         return a-b
 
-print(calculation(3, 5, "+"))
+# print(calculation(3, 5, "+"))
+
+
+# Show Employee details
+def showEmployee(name,salary):
+    print("Employee ",name," salary is:", salary)
+
+showEmployee("Sam", 9000)
 
