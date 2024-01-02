6f7af6be600fa4b57f1ee86ba96ce38012680b91
annamani <annamanik.vit@gmail.com>
2024-01-02 15:27:32 +0100

    concat

--- Day -05/stringconcat.py
+++ Day -05/stringconcat.py
@@ -1,7 +1,6 @@
 first_name=input("Enter First name: ")
 last_name=input("Enter last name: ")
 # print("Hello, "+first_name+" "+last_name)
-age=25
-print(age)
+age=int(input("Enter Age: "))
 print("Hello, "+first_name+" "+last_name)
-print(first_name, " age is : "+age)
+print(first_name, " age is : ",age)
