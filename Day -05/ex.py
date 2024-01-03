64afefcd648eb72d2cba31b4d56d296ba44876c5
annamani <annamanik.vit@gmail.com>
2024-01-03 14:54:22 +0100

    assignment

--- Day -05/ex.py
+++ Day -05/ex.py
@@ -8,3 +8,17 @@ if num1 == num2 and num2 == num3:
     sum = sum * 3
 
 print(sum)
+
+
+num_grade = int(input("Grade: "))
+
+if num_grade >= 90:
+    print("A")
+elif num_grade >= 80:
+    print("B")
+elif num_grade >= 70:
+    print("C")
+elif num_grade >= 60:
+    print("D")
+else:
+    print("F")
