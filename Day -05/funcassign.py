769290fa42445c5d4cd2b5e29c70e9fb88d6bf11
annamani <annamanik.vit@gmail.com>
2024-01-04 21:24:05 +0100

    assignment

--- Day -05/funcassign.py
+++ Day -05/funcassign.py
@@ -21,17 +21,3 @@ def outerFun(a, b):
     return cal(a,b)+8
 result = outerFun(5, 10)
 print(result)
-
-# Palindrome string
-def isPalindrome(string):
-    left_pos = 0
-    right_pos = len(string) - 1
-
-    while right_pos >= left_pos:
-        if not string[left_pos] == string[right_pos]:
-            return False
-        left_pos += 1
-        right_pos -= 1
-    return True
-
-print(isPalindrome('aza'))
