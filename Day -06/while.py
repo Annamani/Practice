e6b46179f478c6ea79e6f17caa494109def6113e
annamani <annamanik.vit@gmail.com>
2024-01-08 19:12:33 +0100

    assignment

--- Day -06/while.py
+++ Day -06/while.py
@@ -1,10 +1,10 @@
 x = 6
-while x < 15:
-	print(x)
-	x += 1
-while x >= 1:
-	print("*" * x)
-	x -= 2
+# while x < 15:
+# 	print(x)
+# 	x += 1
+# while x >= 1:
+# 	print("*" * x)
+# 	x -= 2
 while x < 15:
 	if x % 2 == 0:
 		print("Even:", x)
