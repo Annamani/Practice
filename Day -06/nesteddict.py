2d15a85402380baeb637134af72dda912b89abc6
annamani <annamanik.vit@gmail.com>
2024-01-09 17:18:12 +0100

    assignment

--- Day -06/nesteddict.py
+++ Day -06/nesteddict.py
@@ -1,5 +1,12 @@
 people = {1: {'name': 'John', 'age': '27', 'sex': 'Male'},
           2: {'name': 'Marie', 'age': '22', 'sex': 'Female'}}
+people[3]={}
 # print(people)
 print(people[1])
 print(people[2])
+people[3]['name'] = 'Luna'
+people[3]['age'] = '24'
+people[3]['sex'] = 'Female'
+people[3]['married'] = 'No'
+print(people[3])
+
